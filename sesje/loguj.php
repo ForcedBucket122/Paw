48a07b92867153c9466dc121299dc21accd37b4b
ForcedBucket122 <dzejkob2112@gmail.com>
2024-03-18 12:10:24 +0100

    nienawidze sesji

--- sesje/loguj.php
+++ sesje/loguj.php
@@ -1,35 +1,33 @@
 <?php
-    session_start();
-    if(isset($_SESSION['log'])){
-        header('location: strona.php');
-        exit();
-    }
-    elseif (isset($_POST['nazwa'])&&isset($_POST['haslo'])) {
-        if($_POST['nazwa']=='admin'&& $_POST['haslo']=='admin123')
-    {
-        $_SESSION['log']=$_POST['nazwa'];
-        header('location : strona.php');
-        exit();
-    }
-    else
-    {
-        echo "nieprawidlowe dane logowania";
-    }
-}
-?>
+        session_start();
+        if(isset($_SESSION['log'])){
+            header('location: strona.php');
+            exit();
+        }
+        elseif(isset($_POST['nazwa']) && isset($_POST['haslo'])){
+            if($_POST['nazwa'] == 'admin' && $_POST['haslo'] == 'admin123'){
+                $_SESSION['log'] = $_POST['nazwa'];
+                header('location: strona.php');
+                exit();
+            }
+            else{
+                echo "Nieprawidłowe dane logowania";
+            }
+        }
+    ?>
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pl">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Sesje</title>
 </head>
 <body>
-    <form action="http://localhost/2pb/sesje/strona.php" method="post">
-        <p>logowanie</p>
-        Nazwa uzytkownika: <input type="text" name="nazwa" id=""><br><br>
-        <input type="password" name="haslo" id=""><br><br>
-        <input type="submit" value="zaloguj sie">
+<form method="post" >
+        <p>Logowanie</p>
+        Nazwa użytkownika: <input type="text" name="nazwa" value="" size="25"><br><br>
+        Hasło: <input type="password" name="haslo" value="" size="25"><br><br>
+        <input type="submit" value="Zaloguj się">
     </form>
 </body>
 </html>
--- sesje/strona.php
+++ sesje/strona.php
@@ -1,25 +1,45 @@
 <?php
 session_start();
-if(!isset($_SESSION['log']))
-{
-    header('location: loguj.php');
+if(!isset($_SESSION['log'])){
+    header('location: wyloguj.php');
     exit();
 }
 ?>
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pl">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
 <body>
+    <h1>BAZINGA</h1>
     <?php
-    $imie=ucfirst($_SESSION['log']);
-    echo "witaj ". $imie;
+        $imie = ucfirst($_SESSION['log']);
+        echo "Witaj ".$imie;
     ?>
-    <p>Jestes na stronie glownej</p>
-    <p>przed opuszczeniem strony wyloguj sie</p>
-    <a href="./wyloguj.php">Wyloguj</a>
+    <p>Jesteś na stronie głównej</p>
+    <p>Przed opuszczeniem strony wyloguj się!</p>
+    <a href="wyloguj.php">Wyloguj</a>
+
+    <table>
+        <tr>
+            <td><b>Użytkownicy</td>
+            <td><b>Nazwa</td>
+            <td><b>Hasło</td>
+        </tr>
+
+        <tr>
+            <td></td>
+            <td>Admin</td>
+            <td>admin123</td>
+        </tr>
+
+        <tr>
+            <td></td>
+            <td>Zozi</td>
+            <td>TotalneL</td>
+        </tr>
+    </table>
 </body>
 </html>
